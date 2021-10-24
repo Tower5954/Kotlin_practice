8b5e475914a2aedc527eb61c1841238a5d31f40c
Grant Robert Thompson <Tower5954@github.com>
2021-10-24 05:03:41 +0100

    Initial commit

---
+++ app/src/main/java/com/example/kotlin_three/LearningKotlin.kt
@@ -0,0 +1,61 @@
+package com.example.kotlin_three
+
+fun main(){
+
+
+    // var and val
+
+   // var age : Int = 39
+   // var candy : String
+
+
+    // strings
+
+   // candy = "cadburys"
+
+    // maths in kotlin
+
+   // var wallet : Int = 15 // starting wallet amount
+
+    // wallet = 10 // after shops
+
+    // wallet = 30 // after work
+
+    // wallet = wallet - 10 // after night out
+    // wallet -= 10
+
+    //wallet += 40 // same principal as previous code, just another way of writing it
+
+    //println(wallet)
+
+    var pi = 3.14159265358 // called a 'Double' not a Float
+    var radius = 5
+    
+    var circumference = pi * radius * 2
+    println(circumference)
+
+    var circum = pi.toInt() * radius * 2
+    println(circum)
+
+    var weight = 20
+
+    weight -= 5
+    println(weight)
+
+    // If statements and Booleans
+
+    
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
